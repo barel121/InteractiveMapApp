968618cd18f2a7dec831123f4fcacf16633a84d2
Barel <barel1@windowslive.com>
2025-02-09 12:26:03 +0200

    Added Leaflet map component with basic setup

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css',
+})
+export class MapComponent {
+  private _map!: L.Map;
+
+  ngAfterViewInit() {
+    this._map = L.map('map').setView(
+      [31.264883386785765, 34.81456527201976],
+      13
+    );
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this._map);
+  }
+}
