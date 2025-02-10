c4b89d027100359c4248be0f27f4fb339d5e4c6c
Barel <barel1@windowslive.com>
2025-02-10 19:25:53 +0200

    Removed unwanted imports

--- src/app/components/feature-list/feature-list.component.ts
+++ src/app/components/feature-list/feature-list.component.ts
@@ -2,14 +2,11 @@ import { Component, effect } from '@angular/core';
 import { MapDataService } from '../../services/map-data.service';
 import { DrawnFeature } from '../../models/feature.model';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
 import { MatRippleModule } from '@angular/material/core';
-
 @Component({
   selector: 'app-feature-list',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatListModule, MatRippleModule],
+  imports: [MatIconModule, MatRippleModule],
   templateUrl: './feature-list.component.html',
   styleUrl: './feature-list.component.css',
 })
--- src/app/components/tool-selector/tool-selector.component.ts
+++ src/app/components/tool-selector/tool-selector.component.ts
@@ -3,13 +3,12 @@ import { Tool } from '../../models/tool.model';
 import { toolSet } from '../../utils/tools';
 import { MapDataService } from '../../services/map-data.service';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-tool-selector',
   standalone: true,
-  imports: [MatTooltipModule, MatButtonModule, MatButtonToggleModule],
+  imports: [MatTooltipModule, MatButtonToggleModule],
   templateUrl: './tool-selector.component.html',
   styleUrl: './tool-selector.component.css',
 })
